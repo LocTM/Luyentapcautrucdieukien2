59ed3553eda2507ad44215cac80187b018af4724
LocTM <tranminhloc.anhk@gmail.com>
2023-11-27 17:23:10 +0700

    main

---
+++ index.js
@@ -0,0 +1,20 @@
+// Chuyển từ độ C sang độ F
+// let a = +prompt("Nhập vào đây độ C: ");
+// let result;
+// function changeF(a){
+//     result = a  *  9 / 5 + 32
+//     return result;
+// };
+// changeF(a);
+// console.log(result)
+
+// Chuyển từ mét sang feet:
+// let a = +prompt("Nhập vào đây số mét: ");
+// let b = 3.2808
+// let result;
+// function cal(){
+//     result = a * b;
+//     return result;
+// }
+// cal();
+// console.log(`Số feet tương ứng là: ` + result)
